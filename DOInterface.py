4b3914aac96b0ce7115f41bc5ab3ff37ea7c3b9b
TMRomain <romainover51@gmail.com>
2022-02-23 19:16:24 -0500

    Debut de l'interface

---
+++ DOInterface.py
@@ -0,0 +1,13 @@
+from tkinter import * 
+
+root = Tk()
+
+
+titre = Label(root,text="Download Organiser")
+description = Label(root,text="Script python qui organise le fichier de telechargement")
+
+
+titre.grid(row=0,column=0)
+description.grid(row=1,column=0)
+
+root.mainloop()
