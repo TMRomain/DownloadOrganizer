4b259bcf140e7f434b220b858ab59b24420d33c3
TMRomain <romainover51@gmail.com>
2022-02-23 20:40:14 -0500

    Ajout de commentaire

--- DOInterface.py
+++ DOInterface.py
@@ -7,7 +7,7 @@ import json
 from CleanDownload import cleanUp
 
 
-
+#Fonction qui demande de choisir le fichier de telechargement et qui lance la fonction pour l'enregistrer
 def choisirPath():
     filename = fd.askdirectory()
     if filename !="":
@@ -17,26 +17,26 @@ def choisirPath():
     else:
         cheminText.config(text = "Choisir un Chemin")
 
-
+# Fonction qui lance le script d'organisation du fichier choisir
 def organiser():
     # print()
     cleanUp(cheminText.cget("text"))
 
+# Ecrit le path dans un fichier Json
 def ecrireJson(filepath):
     dictionary ={
         "filepath" : filepath,
     }
     
-    # Serializing json 
     json_object = json.dumps(dictionary)
     
-    # Writing to sample.json
     with open("save.json", "w") as outfile:
         outfile.write(json_object)
 
+
+# Li le json et retourne la premiere valeur
 def lireJson():
     with open(getcwd()+"\\save.json", 'r') as openfile:
-        # Reading from json file
         json_object = json.load(openfile)
 
 
@@ -45,11 +45,11 @@ def lireJson():
 
 
 root = Tk()
-
+# Mettre un titre et une icon
 root.title("Download Organiser")
 root.iconbitmap(getcwd()+"\\images\\icon.ico")
 
-
+# Ajout des element d'interface
 titre = Label(root,text="Download Organiser")
 description = Label(root,text="Script python qui organise les fichiers du dossier 'Telechargement'")
 buttonChoisirPath = Button(root,text="Choisir Votre Fichier De telechargement ",padx = 50, command=choisirPath)
@@ -61,7 +61,7 @@ buttonOrganiser = Button(root,text="Organiser",padx = 50, command=organiser,stat
 
 footer = Label(root,text="Par Romain Dubard Robert")
 
-
+# Placement dans l'interface
 titre.grid(row=0,column=0)
 description.grid(row=1,column=0)
 buttonChoisirPath.grid(row=2,column=0)
@@ -72,6 +72,7 @@ footer.grid(row=5,column=0)
 
 print(os.getenv('APPDATA'))
 
+# Si le fichier de save existe le charge
 if path.exists(getcwd()+"\\save.json"):
     path = lireJson()
     cheminText.config(text = path)
