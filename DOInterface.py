15ff5fe3a347d6786740394898ffd5b3093155bf
TMRomain <romainover51@gmail.com>
2022-02-23 19:23:22 -0500

    Lien entre les deux script

--- DOInterface.py
+++ DOInterface.py
@@ -1,13 +1,14 @@
 from tkinter import * 
 
+from CleanDownload import cleanUp
+
 root = Tk()
 
 
 titre = Label(root,text="Download Organiser")
-description = Label(root,text="Script python qui organise le fichier de telechargement")
+description = Label(root,text="Script python qui organise les fichiers du dossier 'Telechargement'")
 
 
 titre.grid(row=0,column=0)
 description.grid(row=1,column=0)
-
 root.mainloop()
