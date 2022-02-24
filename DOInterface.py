9335d27ec9a27114c83d6ba3d46dcee7d36b900f
TMRomain <romainover51@gmail.com>
2022-02-23 19:26:46 -0500

    Ajout du bouton en lien avec ma fonction de cleanUp

--- DOInterface.py
+++ DOInterface.py
@@ -7,8 +7,13 @@ root = Tk()
 
 titre = Label(root,text="Download Organiser")
 description = Label(root,text="Script python qui organise les fichiers du dossier 'Telechargement'")
+buttonOrganiser = Button(root,text="Organiser",padx = 50, command=cleanUp)
+
 
 
 titre.grid(row=0,column=0)
 description.grid(row=1,column=0)
+buttonOrganiser.grid(row=2,column=0)
+
+
 root.mainloop()
